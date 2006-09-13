97d2ac5e4ffc27860aec45a3e877a92fa16481f9
Stephen Fluin <stephen@mistuph.com>
2006-09-13 20:55:27 +0000

    Added main class.

---
+++ bisbat/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package bisbat;
+
+/**
+ * @author flui0001
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
