8cf8cc35e0c85cfae39a2b0139165d687a4b2d97
Stephen Fluin <stephen@mistuph.com>
2006-11-29 06:32:34 +0000

    Added more data to the guessName functions.

--- bisbat/Being.java
+++ bisbat/Being.java
@@ -8,6 +8,11 @@ public class Being {
 	
 	public String name, shortDesc, longDesc;
 	public int hp, maxhp;
+	
+	/**
+	 * Contains a list of total damages dealt by this mobile for 
+	 * each fight.
+	 */
 	public Vector<Integer> damageHistory;
 	public Vector<Room> seenIn;
 	public int guessLocation = 0;
@@ -42,6 +47,10 @@ public class Being {
 	public String guessName() {
 		String[] names = longDesc.split(" ");
 		if(guessMode == 0) {
+			if(guessLocation >= names.length) {
+				Bisbat.debug("We couldn't figure out the name of the mobile.  There is a chance she left us or her long is misleading");
+				return "!!!Unknown!!!";
+			}
 			return names[guessLocation].substring(0,1);
 		} else {
 			if(guessLocation >= names.length) {
@@ -57,14 +66,24 @@ public class Being {
 			return false;
 		} else {
 			Pattern p = Pattern.compile("(.*) looks much tougher than you\\..*", Pattern.MULTILINE | Pattern.DOTALL);
-			Pattern p2 = Pattern.compile("You are much tougher than (.*)\\..*");
+			Pattern p2 = Pattern.compile("You are much tougher than (.*?)\\..*", Pattern.MULTILINE | Pattern.DOTALL);
 			Matcher m = p.matcher(result);
+			Matcher m2 = p2.matcher(result);
 			if(m.matches()) {
 				//Bisbat.debug("Someone looks much tougher than us, lets grab '" + m.group(1) + "'.");
 				shortDesc = m.group(1);
 				name = stripOfPronouns(shortDesc);
-			} else if(p2.matcher(result).matches()) {
-				shortDesc = p2.matcher(result).group(1);
+			} else if(m2.matches()) {
+				try{
+					shortDesc = m2.group(1);
+				} catch(IllegalStateException e) {
+					e.printStackTrace();
+					Bisbat.debug(result);
+					if(p2.matcher(result).matches()) {
+						Bisbat.debug("Error, even though it matches.");
+					}
+					Bisbat.debug(m2.group());
+				}
 				name = stripOfPronouns(shortDesc);
 				
 			} else {
@@ -101,8 +120,9 @@ public class Being {
 	}
 
 	public static String stripOfPronouns(String string) {
-		String result = string.replaceAll("the ", "");
-		result = result.replaceAll("a ", "");
+		String result = string.replaceAll("[tT]he ", "");
+		result = result.replaceAll("[Aa] ", "");
+		result = result.replaceAll("[Aa]n ", "");
 		return result;
 	}
 	
